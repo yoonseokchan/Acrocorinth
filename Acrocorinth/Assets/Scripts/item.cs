0e92bae61b0773b2b55cd007380bf600f2fdaad0
junhee4613 <junhee000123@naver.com>
2023-06-05 11:46:57 +0900

    ite

---
+++ Acrocorinth/Assets/Scripts/item.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class item : MonoBehaviour
+{
+  public void GamePlayScene()
+    {
+        SceneManager.LoadScene("GamePlayScene");
+    }
+}
